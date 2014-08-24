58724a0f6e2f740c42f7c86104a1512efeff08fc
Ivan Magda <imagda15@gmail.com>
2014-08-24 20:59:20 +1000

    Initial Commit

---
+++ MyNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNotes
+//
+//  Created by Ivan Magda on 24.08.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNotes
+//
+//  Created by Ivan Magda on 24.08.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
