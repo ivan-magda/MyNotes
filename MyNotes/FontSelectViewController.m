4407bcd8fd498f8609a231f367741afb8db2d0e2
Ivan Magda <imagda15@gmail.com>
2014-08-29 10:07:20 +1000

    UIImprovements

--- MyNotes/FontSelectViewController.m
+++ MyNotes/FontSelectViewController.m
@@ -24,14 +24,19 @@
     _textFamily = [UIFont familyNames];
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [_textFamily count];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [_textFamily count];
+    NSArray *fonts = [UIFont fontNamesForFamilyName:_textFamily[section]];
+    return ([fonts count] + 1);
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FontCell"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FontCell" forIndexPath:indexPath];
     
     [self configCell:cell withIndexPath:indexPath];
     
@@ -39,18 +44,35 @@
 }
 
 - (void)configCell:(UITableViewCell *)cell withIndexPath:(NSIndexPath *)indexPath {
-    cell.textLabel.text = _textFamily[indexPath.row];
-    cell.textLabel.font = [UIFont fontWithName:_textFamily[indexPath.row] size:17];
-    cell.textLabel.textColor = [UIColor colorWithRed:self.colorToShow.redColor
-                                               green:self.colorToShow.greenColor
-                                                blue:self.colorToShow.blueColor
-                                               alpha:self.colorToShow.alphaColor];
+    if (indexPath.row == 0) {
+        cell.textLabel.text = _textFamily[indexPath.section];
+        cell.textLabel.font = [UIFont fontWithName:_textFamily[indexPath.row] size:17];
+        cell.textLabel.textColor = [UIColor blackColor];
+    } else {
+        NSArray *styles = [UIFont fontNamesForFamilyName:_textFamily[indexPath.section]];
+        cell.textLabel.text = styles[indexPath.row - 1];
+        cell.textLabel.font = [UIFont fontWithName:styles[indexPath.row - 1] size:17];
+        cell.textLabel.textColor = [UIColor colorWithRed:self.colorToShow.redColor
+                                                   green:self.colorToShow.greenColor
+                                                    blue:self.colorToShow.blueColor
+                                                   alpha:self.colorToShow.alphaColor];
+    }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    [self.delegate fontSelectViewController:self didFinishSelectFont:_textFamily[indexPath.row]];
+    NSArray *styles = [UIFont fontNamesForFamilyName:_textFamily[indexPath.section]];
+    
+    [self.delegate fontSelectViewController:self didFinishSelectFont:styles[indexPath.row - 1]];
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        return nil;
+    } else {
+        return indexPath;
+    }
 }
 
 
